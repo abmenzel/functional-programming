2d219400363bd3bde4521a1794fd78d2f19b96e2
Alexander <alexander@menzel.dk>
2021-04-25 22:03:46 +0200

    finish question 4 jun2018

--- exams/june2018.fsx
+++ exams/june2018.fsx
@@ -47,6 +47,138 @@ let rec map f h =
     | HP(i,l,r) -> HP(f i,map f l, map f r)
     | EmptyHP -> EmptyHP
 
+// Example of function that makes ex3 heap property false
 let exfalse3 = map (fun e -> e*(-1)) ex3
-let test1 = chkHeapProperty ex3
-let test2 = chkHeapProperty exfalse3
+
+// Question 2
+
+// Question 2.1
+
+// Declare function genRandoms n
+let random =
+    let rnd = System.Random()
+    fun () -> rnd.Next(1,10000)
+
+let genRandoms n =
+    [| for _ in 1 .. n -> random() |]
+
+let genRandomsP n =
+    Array.Parallel.init n (fun _ -> random())
+
+genRandoms 100000000
+genRandomsP 100000000
+// Question 2.2
+
+// Declare split ...
+let split xs =
+    match xs with 
+    | [] -> ([], [])
+    | _ -> List.splitAt ((List.length xs)/2) xs
+
+let case1 = split [1;2;3;1]
+// Assert that a simple list with an even number of items is distributed into two lists
+let case2 = split [1;2;3]
+// Assert that a list with an uneven number of items is split into two lists, with the second list getting the uneven index
+let case3 = split ([]:list<int>)
+// Assert that an empty list is split into two empty lists
+
+// Declare indivisible xs
+let indivisible xs =
+    match xs with
+    | [] -> true
+    | [_] -> true
+    | _ -> false
+
+let ind_case1 = indivisible ([]:list<int>)
+let ind_case2 = indivisible [1]
+let ind_case3 = indivisible [1;3;4]
+
+// Define merge
+let merge (xs,ys) =
+    let rec m (list1,list2) merged =
+        match list1, list2 with
+        | [],[] -> merged
+        | x::list1,[] -> m (list1, list2) (x::merged) 
+        | [],y::list2 -> m (list1, list2) (y::merged) 
+        | x::list1, y::list2 when x >= y -> m (x::list1, list2) (y::merged) 
+        | x::list1, y::list2 when x <= y -> m (list1, y::list2) (x::merged) 
+    List.rev (m (xs,ys) [])
+
+let merge_case1 = merge ([],[1])
+// Assert that the function merges with an empty list
+let merge_case2 = merge ([1;2;3], [1;5])
+// Assert that the function places items from list2 in the correct order, that is 1 at index 1 and 5 at index 4
+let merge_case3 = merge ([1;2;3], [1;2;3])
+// Assert that the function correctly merges identical lists
+
+
+// Question 4
+exception FigError of string
+type Point = P of double * double
+type Fig =
+    | Circle of Point * double
+    | Line of Point * Point
+    | Move of double * double * Fig
+    | Combine of Fig list
+    | Label of string * Fig
+    | Ref of string
+
+let figEx01 = Combine [Circle(P(1.0,1.0),2.0);Line(P(0.0,0.0),P(1.0,1.0))]
+
+// Declare an F# value rectEx of type Fig that represents a rectangle with four points
+let rectEx = Combine [Line(P(-1.0,1.0),P(1.0,1.0));Line(P(1.0,1.0),P(1.0,-1.0));Line(P(-1.0,-1.0),P(1.0,-1.0)); Line(P(-1.0,1.0),P(-1.0,-1.0))]
+
+// Declare rect ...
+let rect (x1,y1) (x2,y2) =
+    Combine [Line(P(x1,y1),P(x2,y1)); Line(P(x1,y2),P(x2,y2)); Line(P(x1,y1),P(x1,y2)); Line(P(x2,y1),P(x2,y2))]
+
+// Consider F# value figEx02
+let figEx02 =
+    Combine [Label("c",Circle(P(0.0,0.0),1.0));
+             Move(1.0,1.0,Ref "c");
+             Move(2.0,2.0,Ref "c")]
+
+let figEx03 =
+    Combine [Label("e",Line(P(1.0,1.0),P(2.0,2.0))); Label("d",Line(P(5.0,5.0),P(6.0,6.0)))]
+
+let buildEnv fig =
+    match fig with
+    | Label(l,figure) -> Map [(l,figure)]
+    | Combine(list) ->
+        let rec findLabels ls acc =
+            match ls with
+            | Label(l,figure)::ls -> findLabels ls (Map.add l figure acc)
+            | _::ls -> findLabels ls acc
+            | _ -> acc
+        findLabels list Map.empty
+    | _ -> Map.empty
+
+let envEx02 = buildEnv figEx02
+let envEx03 = buildEnv figEx03
+
+// Question 4.3
+
+// Declare F# function substFigRefs env fig
+let substFigRefs env fig =
+    let rec substFigRefsRec fig =
+        match fig with
+        | Ref(ref) ->  Map.find ref env
+        | Combine(list) -> Combine (List.map substFigRefsRec list)
+        | Label(_,x) -> substFigRefsRec x
+        | Move(x,y,z) -> Move(x,y,substFigRefsRec z)
+        | _ -> fig
+    substFigRefsRec fig 
+
+let substEx02 = substFigRefs envEx02 figEx02
+
+// Question 4.4
+
+let rec reduceMove fig =
+    match fig with
+    | Move(newX,newY,Circle(P(x,y),r)) -> Circle(P(x+newX,y+newY),r)
+    | Move(newX,newY,Line(P(x,y),P(x1,y1))) -> Line(P(x+newX,y+newY),P(x1+newX,y1+newY))
+    | Combine(list) -> Combine (List.map reduceMove list)
+    | _ -> fig
+
+let reduceEx02 = reduceMove substEx02
+    
